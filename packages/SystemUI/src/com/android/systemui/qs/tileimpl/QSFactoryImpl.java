fa3849857fdfbf4d4fdc8bd8c50c41b175edcf7c
Adam Szorenyi <adam.szorenyi88@gmail.com>
2021-03-07 12:09:57 +0100

    Backport VPN QS Tile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -58,6 +58,7 @@ import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.VolumeTile;
+import com.android.systemui.qs.tiles.VpnTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.qs.QSTileHost;
@@ -166,6 +167,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CPUInfoTile(mHost);
             case "onthego":
                 return new OnTheGoTile(mHost);
+            case "vpn":
+                return new VpnTile(mHost);
         }
 
         // Intent tiles.
